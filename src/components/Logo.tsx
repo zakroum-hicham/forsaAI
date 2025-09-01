7403f244e8d1b90519d47661a4517d17397df0c3
Hicham Zakroum <hicham.zakroum2@gmail.com>
2025-09-01 18:11:36 +0200

    refactor: make the logo a Link

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,13 +1,16 @@
 import { Brain } from "lucide-react";
+import Link from "next/link";
 
 const Logo = () => {
   return (
+    <Link href="/">
     <div className="flex items-center space-x-2">
         <div className="w-8 h-8 bg-gradient-to-r from-blue-600 to-purple-600 rounded-lg flex items-center justify-center">
         <Brain className="w-5 h-5 text-white" />
         </div>
         <span className="text-xl font-bold text-slate-900">ForsaAI</span>
       </div>
+      </Link>
   );
 
 };
